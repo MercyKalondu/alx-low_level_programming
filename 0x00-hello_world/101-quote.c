37b7ec54f12370fcad53f828f8ae6b2e68b19d62
MercyKalondu <mercykalondu62@gmail.com>
2022-06-02 06:09:02 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main = Entry point
  *
  * Return: Always 0 (Success)
  */
 {
-	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",89);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",89);
 
-	return (1)
+	return (1);
 }
