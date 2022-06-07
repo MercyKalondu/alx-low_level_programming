1bb56bb84149592a7cbe944786544c8618c644da
MercyKalondu <mercykalondu62@gmail.com>
2022-06-07 03:33:48 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,10 @@
  */
 int main(void)
 {
-	char *sh = "Main";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('m');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
