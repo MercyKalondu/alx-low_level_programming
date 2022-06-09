1d98f47754610b60f1078e5f6759cd01e9063bf3
MercyKalondu <mercykalondu62@gmail.com>
2022-06-09 03:21:15 -0700

    Print number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - function that prints the numbers, from 0 to 9, followed by a new line
- * Return: no return
+ * Return: 0-9 followed by newline
  */
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar((i % 10) + '0');
+		_putchar(i + '0');
 
 	_putchar('\n');
 }
