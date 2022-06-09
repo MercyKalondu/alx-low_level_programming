05f4b471ea5014e04b7a52cd7e7e1b5f45a05964
MercyKalondu <mercykalondu62@gmail.com>
2022-06-09 03:50:23 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,10 +3,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: Print the character
+ * @c: The character to print
  *
- * Return: 1 (Success)
- * On error, -1 is returned, and error is set appropriately
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
