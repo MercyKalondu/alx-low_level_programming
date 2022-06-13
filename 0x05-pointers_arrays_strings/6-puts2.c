ce98b9aed16d8a5ae3a6fbc32cd4bd0e4838cd9b
MercyKalondu <mercykalondu62@gmail.com>
2022-06-13 05:05:43 -0700

    Puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every other character of a string
+ * puts2 - prints every other character of a  string to stdout
  * @str: input string
+ *
  * Return: nothing
  */
 void puts2(char *str)
