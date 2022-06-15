b401c5736eef06d36df4bbad2e09bef84dd5f28d
MercyKalondu <mercykalondu62@gmail.com>
2022-06-15 10:23:21 -0700

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,28 +2,25 @@
 
 /**
  * rot13 - a function that encodes a string using rot13
- * @n: input
+ * @s: input
  * Return: A pointer to the encoded string
  */
-char *rot13(char *n)
+char *rot13(char *s)
 {
-	int x, rot_c = 13, i = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v', 'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M', 'Z', 'm', 'z'};
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (n[i] != '\0')
+	for (i = 0; *(s + i); i++)
 	{
-		for (x = 0; x <= 51; x++)
+		for (j = 0; j < 52; j++)
 		{
-			if (n[i] == toswap[x])
+			if (a[j] == *(s + i))
 			{
-				n[i] = n[i] + rot_c;
-				x = 51;
+				*(s + i) = b[j];
+				break;
 			}
-			rot_c = rot_c * -1;
 		}
-		i++;
 	}
-	return (n);
+	return (s);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ char *string_toupper(char *m);
 char *cap_string(char *s);
 char *leet(char *n);
 int _putchar(char c);
-char *rot13(char *n);
+char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
