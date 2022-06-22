ddb69a4f18949f1287c9c75bc9f3476803ae1a84
MercyKalondu <mercykalondu62@gmail.com>
2022-06-22 05:52:01 -0700

    Compare string

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * wildcmp - Entry point
+ * @s1: input
+ * @s2: input
+ * Return: Always 0 (Success)
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
