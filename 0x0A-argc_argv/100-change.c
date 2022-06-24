47f04c61c83fc7bc40c9c00ec27801fd1b8e502a
MercyKalondu <mercykalondu62@gmail.com>
2022-06-24 08:10:24 -0700

    Minimum change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,30 +8,40 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, ccoins = 0;
+	int cents, coins = 0;
 
-	if (argc == 1 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
 
 	while (cents > 0)
 	{
-		if (cents >= 25)
-			cent -= 25;
-		else if (cents >= 10)
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
 			cents -= 10;
-		else if (cents >= 5)
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
 			cents -= 5;
-		else if (cents >= 2)
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
 			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ccoins += 1;
+			continue;
+		}
+		cents--;
 	}
-	printf("%d\n", ccoins);
+	printf("%d\n", coins);
 	return (0);
 }
