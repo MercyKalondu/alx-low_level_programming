ab62c9b30e3cb66b433a9fd0edb8f1335679756c
MercyKalondu <mercykalondu62@gmail.com>
2022-06-27 05:54:38 -0700

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,14 +16,14 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	
-	array = malloc(sizeof(c) * size);
-
+	array = malloc(size * sizeof(char));
 	if (array == NULL)
+	{
 		return (NULL);
-
+	}
 	for (i = 0; i < size; i++)
+	{
 		array[i] = c;
-
+	}
 	return (array);
 }
