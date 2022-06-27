e1c65b294ad0bd35591af7c12ceb32dfa6a89d09
MercyKalondu <mercykalondu62@gmail.com>
2022-06-27 06:00:07 -0700

    New and copy

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,22 +10,22 @@
  */
 char *_strdup(char *str)
 {
-	char *strout;
 	unsigned int i, j;
+	char *p;
 
 	if (str == NULL)
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i]; i++)
 		;
-
-	strout = (char *)malloc(sizeof(char) * (i + 1));
-
-	if (strout == NULL)
+	i++;
+	p = malloc(i * sizeof(char));
+	if (s == NULL)
+	{
 		return (NULL);
-	
-	for (j = 0; j <= i; j++)
-		strout[j] = str[j];
-
-	return (strout);
+	}
+	for (j = 0; j < i; j++)
+	{
+		p[j] = str[j];
+	}
+	return (p);
 }
